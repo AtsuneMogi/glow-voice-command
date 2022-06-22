8275bac8d656eab18649f23903c0d167889b0759
Atsune MOGI <77053345+AtsuneMogi@users.noreply.github.com>
2022-06-23 00:38:03 +0900

    Update ser.py

--- python/ser.py
+++ python/ser.py
@@ -1,12 +1,15 @@
 import serial
 import time
+import requests
+from bs4 import BeautifulSoup
+
+ser = serial.Serial("COM3", 115200, timeout=None)
+(func, group, comm) = (1, 1, 30)
+id = [1, 2, 3, 4]
+i = 1
 
 
-def main():
-  ser = serial.Serial("COM3", 115200, timeout=None)
-  (func, group, comm) = (1, 1, 30)
-  id = [1, 2, 3, 4]
-  i = 1
+def glowOn():
   try:
     while 1:
       message = (str(func)+","+str(group)+","+str(id[i-1])+","+str(comm)).encode('utf-8')
@@ -16,15 +19,36 @@ def main():
       time.sleep(2)
       print(message)
   except KeyboardInterrupt:
-    for i in id:
-      time.sleep(1.5)
-      message = (str(func)+","+str(group)+","+str(i)+","+str(0)).encode('utf-8')
-      ser.write(message)
-      print(message)
-    time.sleep(1.5)
+    glowOff()
   ser.close()
   return
-  
+
+
+def glowOff():
+  for i in id:
+    time.sleep(1.5)
+    message = (str(func)+","+str(group)+","+str(i)+","+str(0)).encode('utf-8')
+    ser.write(message)
+    print(message)
+  time.sleep(1.5)
+
+
+def main():
+  while(1):
+    getContent()
+    time.sleep(2)
+
+
+def getContent():
+  url = "https://atsunemogi.github.io/glow-voice-command/"
+  html = requests.get(url)
+  soup = BeautifulSoup(html.content, "html.parser")
+  command = soup.find(id="content").text
+  if ((command == "one") or (command == "1")):
+    glowOn()
+  elif ((command == "zero") or (command == "0")):
+    glowOff()
+
 
 if __name__ == '__main__':
   main()
