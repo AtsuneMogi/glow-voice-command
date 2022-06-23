f9426f9cbc38fb5db3379754818943cfa0fb603a
Atsune MOGI <77053345+AtsuneMogi@users.noreply.github.com>
2022-06-23 15:31:50 +0900

    Update ser.py

--- python/ser.py
+++ python/ser.py
@@ -1,54 +1,62 @@
 import serial
 import time
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome import service
+from selenium.webdriver.common.by import By
 
 ser = serial.Serial("COM3", 115200, timeout=None)
 (func, group, comm) = (1, 1, 30)
 id = [1, 2, 3, 4]
-i = 1
+# reference: https://www.seleniumqref.com/api/python/element_infoget/Python_text.html
+chromeDriver = "D:\\Users\\USER\\AppData\\Local\\Programs\\chromedriver\\chromedriver.exe"
+chromeService = service.Service(executable_path=chromeDriver)
+driver = webdriver.Chrome(service=chromeService)
+driver.get("https://atsunemogi.github.io/glow-voice-command/")
 
 
 def glowOn():
-  try:
-    while 1:
-      message = (str(func)+","+str(group)+","+str(id[i-1])+","+str(comm)).encode('utf-8')
-      ser.write(message)
-      i = (i + 1) % len(id)
-      comm = (comm + 50) % 1000
-      time.sleep(2)
-      print(message)
-  except KeyboardInterrupt:
-    glowOff()
-  ser.close()
+  global func, group, comm, id
+  for i in id:
+    message = (str(func)+","+str(group)+","+str(i)+","+str(comm)).encode('utf-8')
+    ser.write(message)
+    comm = (comm + 50) % 1000
+    time.sleep(2)
+    print(message)
   return
 
 
 def glowOff():
+  global func, group, comm, id
   for i in id:
     time.sleep(1.5)
     message = (str(func)+","+str(group)+","+str(i)+","+str(0)).encode('utf-8')
     ser.write(message)
     print(message)
-  time.sleep(1.5)
+  time.sleep(2)
+  return
+
+
+def getContent():
+  element = driver.find_element(By.ID, "content")
+  text = element.text
+  if (text == "one") or (text == "1"):
+    print(text)
+    glowOn()
+  elif (text == "zero") or (text == "0"):
+    print(text)
+    glowOff()
 
 
 def main():
   while(1):
-    getContent()
-    time.sleep(2)
-
-
-def getContent():
-  url = "https://atsunemogi.github.io/glow-voice-command/"
-  html = requests.get(url)
-  soup = BeautifulSoup(html.content, "html.parser")
-  command = soup.find(id="content").text
-  if ((command == "one") or (command == "1")):
-    glowOn()
-  elif ((command == "zero") or (command == "0")):
-    glowOff()
-
+    try:
+      getContent()
+      time.sleep(2)
+    except KeyboardInterrupt:
+      glowOff()
+      break
+  ser.close()
+  
 
 if __name__ == '__main__':
   main()
